d1bb891b6005858873ba83227be3c9ccecf93014
PleaBanshee <llewellynant@gmail.com>
2022-07-28 11:23:20 +0200

    Tried fixing flash messaging

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -120,27 +120,22 @@ def create_app(test_config=None):
                 return redirect(request.url)
 
     # Upload Folder
-    @app.route('/ExtensiveText', methods=['POST'])
+    @app.route('/ExtensiveText', methods=['GET','POST'])
     def upload_folder():
         if request.method == 'POST':
-
             if 'files[]' not in request.files:
                 flash('No file part')
                 return redirect(request.url)
-
             files = request.files.getlist('files[]')
-
             for file in files:
                 if file and allowed_file(file.filename):
                     filename = secure_filename(file.filename)
                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                     # this uploads second file
-                    file2 = request.files['file2']
-                    filename2 = secure_filename(file2.filename)
-                    file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
-
-
-            flash('File(s) successfully uploaded')
+                    comparison = request.files['comparison']
+                    filename3 = secure_filename(comparison.filename)
+                    comparison.save(os.path.join(app.config['UPLOAD_FOLDER'], filename3))
+            # flash('File(s) successfully uploaded')
             return redirect('/home')
 
     return app
