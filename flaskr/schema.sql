9dbb7b48e44cd4294f67c38fec2151097aeded1b
Hano Strydom <hanostrydom8@gmail.com>
2022-09-11 10:47:28 +0200

    Changes the SQL Query

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -10,4 +10,4 @@ CREATE TABLE accounts (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- password is admin
-INSERT INTO user VALUES (1,'admin','pbkdf2:sha256:260000$j2dCQrXnaDgBrbmO$896a6fe3480ca5cca5a433f78a52dc123ecd81c479620e5bb179de0cce115fef');
+INSERT INTO accounts VALUES (1,'admin','pbkdf2:sha256:260000$j2dCQrXnaDgBrbmO$896a6fe3480ca5cca5a433f78a52dc123ecd81c479620e5bb179de0cce115fef');
