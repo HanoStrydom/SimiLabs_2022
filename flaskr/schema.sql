e426711de3d8687073b6471db028be1fa3b4920d
PleaBanshee <llewellynant@gmail.com>
2022-09-11 10:18:57 +0200

    Migrated successfully to MySQL

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,12 +1,13 @@
-DROP TABLE IF EXISTS user;
+CREATE DATABASE IF NOT EXISTS SimiLabs DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE Similabs;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
-);
+DROP TABLE IF EXISTS accounts;
 
-
+CREATE TABLE accounts (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  username nvarchar(55) UNIQUE NOT NULL,
+  password nvarchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- password is admin
 INSERT INTO user VALUES (1,'admin','pbkdf2:sha256:260000$j2dCQrXnaDgBrbmO$896a6fe3480ca5cca5a433f78a52dc123ecd81c479620e5bb179de0cce115fef');
