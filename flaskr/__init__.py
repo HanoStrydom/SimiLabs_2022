687844de50fe83a5d92d93b1b7cdc6fa554c3a19
MichaelRosin <michaeljoshuarosin@gmail.com>
2022-11-10 18:46:20 +0200

    Modified Extensive Text page

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -466,21 +466,29 @@ def create_app(test_config=None):
     # Upload Folder
     @app.route('/ExtensiveText', methods=['GET','POST'])
     def upload_folder():
+        # if 'loggedin' not in session:
+        #     return render_template('auth/authLogin.html')
+        # if request.method == 'POST':
+        #     if 'files[]' not in request.files:
+        #         flash('No file part')
+        #         return redirect(request.url)
+        #     files = request.files.getlist('files[]')
+        #     for file in files:
+        #         if file and allowed_file(file.filename):
+        #             filename = secure_filename(file.filename)
+        #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #             # this uploads second file
+        #             comparison = request.files['comparison']
+        #             filename3 = secure_filename(comparison.filename)
+        #             comparison.save(os.path.join(app.config['UPLOAD_FOLDER'], filename3))
+        #     return redirect('/extensiveReport')
         if 'loggedin' not in session:
-            return render_template('auth/authLogin.html')
+                return render_template('auth/authLogin.html')
         if request.method == 'POST':
-            if 'files[]' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            files = request.files.getlist('files[]')
-            for file in files:
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    # this uploads second file
-                    comparison = request.files['comparison']
-                    filename3 = secure_filename(comparison.filename)
-                    comparison.save(os.path.join(app.config['UPLOAD_FOLDER'], filename3))
-            return redirect('/extensiveReport')
+            stdNum = request.form['stdNum']
+            stdName = request.form['stdName']
+            print(stdNum)
+            print(stdName)
+        return render_template('reports/extensiveReport.html')
 
     return app
